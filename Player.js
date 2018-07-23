aa301ed644f82c61cf3d34dc276de23674940365
Aleksander Szczęsny <aleksander03@gmail.com>
2018-07-23 10:48:41 +0200

    2??

--- Player.js
+++ Player.js
@@ -8,7 +8,7 @@ class Player {
     
     console.log(gameState); 
     console.log(gameState.players.Scriptex.hole_cards[0].suit);
-    if (gameState.players.Scriptex.hole_cards[0].suit = gameState.players.Scriptex.hole_cards[1].suit) {
+    if (gameState.players.Scriptex.hole_cards[0].suit == gameState.players.Scriptex.hole_cards[1].suit) {
       bet(2);
     }else{
       bet(0);
