c8c6d762ef261a447fcd951cdf0871ce7453eff9
Marcin Nalewajko <bedgaar@gmail.com>
2018-07-23 11:39:48 +0200

    community log

--- Player.js
+++ Player.js
@@ -8,7 +8,7 @@ class Player {
     
     console.log(gameState); 
     console.log(gameState.players[0].hole_cards[0].suit);
-    gameState.community_cards.forEach(element => {consol.log(gameState.community_cards[element].rank)});
+    gameState.community_cards.forEach(element => {console.log(gameState.community_cards[element].rank)});
     if (gameState.players.players[0].hole_cards[0].rank == gameState.players[0].hole_cards[1].rank||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players.players[0].hole_cards[0].rank})||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players.players[0].hole_cards[0].rank}))
       
     {
