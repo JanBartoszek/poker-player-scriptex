9c20cac9d1c0369927d2b66932444d756e8edfc6
Aleksander Szczęsny <aleksander03@gmail.com>
2018-07-23 10:43:16 +0200

    cos 1

--- Player.js
+++ Player.js
@@ -5,10 +5,14 @@ class Player {
   
   static betRequest(gameState, bet) {
     console.log('///');
-    state=JSON.parse(gameState); 
     
-    console.log(state); 
-    bet(0);
+    console.log(gameState); 
+    
+    if (gameState.players.Scriptex.hole_cards[0].suit = gameState.players.Scriptex.hole_cards[1].suit) {
+      bet(2);
+    }else{
+      bet(0);
+    }
   }
 
   static showdown(gameState) {
