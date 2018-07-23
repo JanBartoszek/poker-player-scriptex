dd7a9a832842361225855c95b885b9ce859a7931
Aleksander Szczęsny <aleksander03@gmail.com>
2018-07-23 12:07:22 +0200

    set to all in

--- Player.js
+++ Player.js
@@ -7,15 +7,8 @@ class Player {
     console.log('///');
     
     console.log(gameState); 
-    console.log(gameState.players[0].hole_cards[0].rank);
-    gameState.community_cards.forEach(element => {console.log(gameState.community_cards[element].rank)});
-    if (gameState.players[0].hole_cards[0].rank == gameState.players[0].hole_cards[1].rank||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players[0].hole_cards[0].rank})||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players[0].hole_cards[0].rank}))
-      
-    {
-      bet(1000);
-    }else{
-      bet(250);
-    }
+    console.log(gameState.players[0].hole_cards[0].suit);
+    bet(2000);
   }
 
   static showdown(gameState) {
