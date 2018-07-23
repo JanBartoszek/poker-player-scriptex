3ab7814400f18342badc65e068f7a0d03fd07b66
Marcin Nalewajko <bedgaar@gmail.com>
2018-07-23 15:27:24 +0200

    bighash

--- Player.js
+++ Player.js
@@ -8,58 +8,49 @@ class Player {
 
   }
   static betRequest(gameState, bet) {
-    console.log('///------------------------------------------------------!!!!!!!');
-    var winningCards = ['A', 'K', 'Q', 'J'];
-    console.log(gameState); 
-    console.log(gameState.players[0].hole_cards[0].rank);
-    console.log(gameState.players[0].hole_cards[1].rank);
-    try{
-      console.log(gameState.community_cards[0].rank + 'CARD COMMUNITY');
-      console.log(gameState.community_cards[1].rank + 'CARD COMMUNITY');
-      console.log(gameState.community_cards[2].rank + 'CARD COMMUNITY');
-    } catch (err) {
-        console.log(err + 'ERRROR!!!');
-    };
+    myBet(bet, 6000);
+    // console.log('///------------------------------------------------------!!!!!!!');
+    // var winningCards = ['A', 'K', 'Q', 'J'];
+    // console.log(gameState); 
+    // console.log(gameState.players[0].hole_cards[0].rank);
+    // console.log(gameState.players[0].hole_cards[1].rank);
+    // try{
+    //   console.log(gameState.community_cards[0].rank + 'CARD COMMUNITY');
+    //   console.log(gameState.community_cards[1].rank + 'CARD COMMUNITY');
+    //   console.log(gameState.community_cards[2].rank + 'CARD COMMUNITY');
+    // } catch (err) {
+    //     console.log(err + 'ERRROR!!!');
+    // };
 
     
-    var potValue = gameState.pot;
-    console.log(potValue + ' !!!!!VVVVVVVALUEPOT');
+    // var potValue = gameState.pot;
+    // console.log(potValue + ' !!!!!VVVVVVVALUEPOT');
     
-    var firstCard = gameState.players[0].hole_cards[0].rank;
-    var secondCard = gameState.players[0].hole_cards[1].rank;
-    console.log(firstCard);
-    console.log(secondCard);
-    try{  
-      if (winningCards.includes(firstCard) || winningCards.includes(secondCard)) {
-        console.log('entered first if');
-<<<<<<< HEAD
-        myBet(bet, Math.round(potValue / 2));
+    // var firstCard = gameState.players[0].hole_cards[0].rank;
+    // var secondCard = gameState.players[0].hole_cards[1].rank;
+    // console.log(firstCard);
+    // console.log(secondCard);
+    // try{  
+    //   if (winningCards.includes(firstCard) || winningCards.includes(secondCard)) {
+    //     console.log('entered first if');
+    //   myBet(bet,6000);
+    //     console.log(bet + ' We bet')
 
+    //   } else if (firstCard == secondCard) {
+    //     console.log('entered second if');
+    //     myBet(bet,6000);
 
-      } else if (firstCard == secondCard) {
-        console.log('entered second if');
-        myBet(bet, potValue + 10);
+    //   } else {
+    //     console.log('entered else');
+    //     myBet(bet, 0);
 
-=======
-      myBet(bet,6000);
-        console.log(bet + ' We bet')
+    //   }
+    // }
+    // catch (err) {
+    //   console.log(err + 'ERRROR!!!');
+    //   myBet(bet, 6000);
 
-      } else if (firstCard == secondCard) {
-        console.log('entered second if');
-        myBet(bet,6000);
->>>>>>> 6f998b5865b0bc53b5bd2cd84f9be47f36c61cfc
-
-      } else {
-        console.log('entered else');
-        myBet(bet, 0);
-
-      }
-    }
-    catch (err) {
-      console.log(err + 'ERRROR!!!');
-      myBet(bet, 6000);
-
-    }
+    // }
   }
 
   static showdown(gameState) {
