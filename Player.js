1f7244e69fe1dd863594410ae649a9d0ef685769
Aleksander Szczęsny <aleksander03@gmail.com>
2018-07-23 13:32:54 +0200

    again to all in

--- Player.js
+++ Player.js
@@ -7,21 +7,18 @@ class Player {
     console.log('///');
     
     console.log(gameState); 
-<<<<<<< HEAD
     console.log(gameState.players[0].hole_cards[0].rank);
     console.log(gameState.community_cards)
-    gameState.community_cards.forEach(element => {console.log(gameState.community_cards[element].rank)});
-    if (gameState.players[0].hole_cards[0].rank == gameState.players[0].hole_cards[1].rank||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players[0].hole_cards[0].rank})||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players[0].hole_cards[0].rank}))
+    // gameState.community_cards.forEach(element => {console.log(gameState.community_cards[element].rank)});
+    // if (gameState.players[0].hole_cards[0].rank == gameState.players[0].hole_cards[1].rank||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players[0].hole_cards[0].rank})||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players[0].hole_cards[0].rank}))
       
-    {
-      bet(1000);
-    }else{
-      bet(250);
-    }
-=======
-    console.log(gameState.players[0].hole_cards[0].suit);
-    bet(2000);
->>>>>>> dd7a9a832842361225855c95b885b9ce859a7931
+    // {
+    //   bet(1000);
+    // }else{
+    //   bet(250);
+    // }
+    // console.log(gameState.players[0].hole_cards[0].suit);
+    bet(5001);
   }
 
   static showdown(gameState) {
