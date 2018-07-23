a473ccf44f6ec76191f98c41cf825bd4a20db8cd
Aleksander Szczęsny <aleksander03@gmail.com>
2018-07-23 11:22:13 +0200

    zmiana na rank

--- Player.js
+++ Player.js
@@ -8,10 +8,10 @@ class Player {
     
     console.log(gameState); 
     console.log(gameState.players[0].hole_cards[0].suit);
-    if (gameState.players.players[0].hole_cards[0].suit == gameState.players[0].hole_cards[1].suit) {
-      bet(2);
+    if (gameState.players.players[0].hole_cards[0].rank == gameState.players[0].hole_cards[1].rank) {
+      bet(4);
     }else{
-      bet(0);
+      bet(2);
     }
   }
 
