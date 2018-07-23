dce6b4a0aac89fbde65b8db69fea7d4260781d00
Marcin Nalewajko <bedgaar@gmail.com>
2018-07-23 11:27:47 +0200

    checking pairs

--- Player.js
+++ Player.js
@@ -8,7 +8,9 @@ class Player {
     
     console.log(gameState); 
     console.log(gameState.players[0].hole_cards[0].suit);
-    if (gameState.players.players[0].hole_cards[0].rank == gameState.players[0].hole_cards[1].rank) {
+    if (gameState.players.players[0].hole_cards[0].rank == gameState.players[0].hole_cards[1].rank||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players.players[0].hole_cards[0].rank})||gameState.community_cards.forEach(element => {gameState.community_cards[element].rank==gameState.players.players[0].hole_cards[0].rank}))
+      
+    });) {
       bet(4);
     }else{
       bet(2);
