9db97c575e247bc432da14993cb162c245f21644
Marcin Nalewajko <bedgaar@gmail.com>
2018-07-23 10:27:49 +0200

    console log

--- Player.js
+++ Player.js
@@ -2,8 +2,11 @@ class Player {
   static get VERSION() {
     return '0.1';
   }
-
+  
   static betRequest(gameState, bet) {
+    state=JSON.parse(gameState); 
+    console.log('///')
+    console.log(state); 
     bet(0);
   }
 
